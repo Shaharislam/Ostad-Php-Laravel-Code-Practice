8640b835b86a95fd18f2740ff20ff484cc050efb
Rakibul Islam Shahar <78203754+Shaharislam@users.noreply.github.com>
2024-10-01 20:35:03 -0700

    Php First Class
    Php First Class

---
+++ Php-Code/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html vs php</title>
+</head>
+<body>
+    <h1>Hello From Php</h1>
+    <?php 
+echo "Hello Zuveria\n";  // Output the greeting
+
+// Declare variables
+$numberOne = 10;  // integer
+$numberTwo = 20;  // integer
+$numberTwo = 50;
+echo $numberOne;
+echo "\n";
+echo $numberTwo;
+echo "fgfg";
+
+echo "\n";
+echo $numberTwo;
+
+echo "\n";
+echo $numberOne * $numberTwo;
+
+const PI = 3.1416;
+
+echo "\n";
+echo $numberOne * PI; ?>
+
+<?php 
+  $item ="NoteBook";
+  $price = 100;
+  echo "</br>";
+  echo "The price of the $item is $price";
+?>
+</body>
+</html>
---
+++ Php-Code/test.php
@@ -0,0 +1,18 @@
+<?php 
+echo "Hello Zuveria\n";  // Output the greeting
+
+// Declare variables
+$numberOne = 10;  // integer
+$numberTwo = 20;  // integer
+$numberTwo = 50;
+echo $numberOne;
+echo "\n";
+echo $numberTwo;
+echo "\n";
+echo $numberTwo;
+echo "\n";
+echo $numberOne * $numberTwo;
+
+const PI = 3.1416;
+echo "\n";
+echo $numberOne * PI;
